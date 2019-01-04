1c9260eab9552470d73fccb7082a63b53f5964a9
Darien Imai <darien@dpsi.jp>
2019-01-03 21:33:04 -0800

    Make some sort of hierarchy

--- Server.py
+++ Server.py
@@ -1,23 +1,46 @@
-from twisted.web import server, resource
-from twisted.internet import reactor, endpoints
-
+from twisted.web.server import Site
+from twisted.internet import reactor
+from twisted.web.resource import Resource
 from LControl import LControl
-import time
 
-control = LControl()
-control.serve_forever()
 
-class Simple(resource.Resource):
+class Quit(Resource):
+    def render_GET(self, request):
+        control.q.put("quit")
+        return b"Quitting..."
+
+
+class Start(Resource):
+    def render_GET(selfself, request):
+        control.serve_forever()
+        return b'Starting...'
+
+
+class Move(Resource):
     isLeaf = True
 
     # A simple example of using the query params as a way to get x/y coords.
     def render_GET(self, request):
-    	if b'x' in request.args and b'y' in request.args:
-	        control.q.put((request.args[b'x'][0].decode('ASCII'), request.args[b'y'][0].decode('ASCII')))
-	        return b"Moving to x= %s y= %s" %(request.args[b'x'][0], request.args[b'y'][0])
+        if b'x' in request.args and b'y' in request.args:
+            control.q.put((request.args[b'x'][0].decode('ASCII'), request.args[b'y'][0].decode('ASCII')))
+            return b"Moving to x= %s y= %s" % (request.args[b'x'][0], request.args[b'y'][0])
+        else:
+            request.setResponseCode(500)
+            return b'Not Implemented'
 
 
-site = server.Site(Simple())
-endpoint = endpoints.TCP4ServerEndpoint(reactor, 8080)
-endpoint.listen(site)
+class LControlRoot(Resource):
+    def getChild(self, name, request):
+        if name == b'quit':
+            return Quit()
+        if name == b'start':
+            return Start()
+        return Move()
+
+
+control = LControl()
+control.serve_forever()
+root = LControlRoot()
+factory = Site(root)
+reactor.listenTCP(8080, factory)
 reactor.run()
